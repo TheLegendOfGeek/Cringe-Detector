9dc5342d904c568bf07f9e494e0108dde1e26107
TheLegendOfGeek <122482386+TheLegendOfGeek@users.noreply.github.com>
2023-01-26 00:00:35 +0100

    Add files via upload

---
+++ cringe_detector.py
@@ -0,0 +1,24 @@
+class Laugh():
+
+    def make_laugh():
+
+     # the variables that the function uses
+        blague = ()
+        cringe_word = []
+        found_cringe = False
+     #Here is the functions that detects cringe words.  
+        for item in cringe_word:
+            if item in blague:
+                found_cringe = True
+        if found_cringe:
+            print("you are cringe")
+        else:
+            print("thats funny")
+
+        
+
+    make_laugh()   
+            
+        
+            
+   
